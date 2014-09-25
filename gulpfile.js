13f6b96cd91fa1979bf1cd9c1f430f8c94d62dc9
jmadden85 <jmadden85@gmail.com>
2014-09-25 18:18:29 -0400

    updating

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,26 @@
 var gulp = require('gulp');
 var watch = require('gulp-watch');
 var git = require('gulp-git');
+var runSequence = require('run-sequence');
 
-gulp.task('build', function () {
+gulp.task('watch', function (cb) {
+    watch('./*', function (files, cb) {
+        // gulp.start('commit', cb);
+        runSequence('commit', 'push', cb);
+    });
+});
+
+gulp.task('commit', function () {
   var date = new Date();
   return gulp.src('./*.psd')
     .pipe(git.add())
     .pipe(git.commit(date));
-    git.push('origin', 'master', function (err) {
-      console.log('test');
-      if (err) throw err;
-    });
 });
 
-gulp.task('watch', function () {
-    watch('./*', function (files, cb) {
-        gulp.start('build', cb);
-    });
+gulp.task('push', function(){
+  git.push('origin', 'master', function (err) {
+    if (err) throw err;
+  });
 });
 
 gulp.task('default', ['watch']);
